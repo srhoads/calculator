df481c58bfa261a49705173d6a7542ebd3873de3
Samantha Karlaina Rhoads <skr2150@columbia.edu>
2019-08-02 18:17:42 -0400

    added some comments & questions for ale
    wherever there's a comment that has 'alejandro' in it, that means i didn't understand something or didnt google it & thus remain unenlightened

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ NOTES: (+ potentially Qs for alejandro)
 - why does it seem like I need to have my board react component class (BoardRC) start with a capital letter? Am i delusional or does it muck up when it's a lowercase b?
 - what are the based regex-ish syntaxes for operations like .replace() & stuff? 
     ˚ie: "|" vs "||" or "\b" or "[[:alpha:]]" or "^$" or "\\<\\>" & more that seem not to work the same way as they do in Python & R
-*/
+- besides inside react component classes, is render() used for anything else ever?
+
+- why do some people's apps have "export" instead of extend React.Com... & sometimes export is at the end. 
+    ˚ it seems like export is used when another script wants to source that given module
+- when do things need to be inside a component & when not? ie: the factorial fxn right below this
+    */
 
 function factorialFXN(num) {
     if (num < 0) return ((num * -1) * factorialFXN((num * -1) - 1)) * -1; //return -1;  // If the number is less than 0, reject it.
@@ -57,9 +62,6 @@ class BoardRC extends React.Component {
                     <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
                     <button className="sideofsquareCSS" name="!" onClick={e => this.props.onClick(e.target.name)}>!</button>
                     <button className="sideofsquareCSS" name=" abs" onClick={e => this.props.onClick(e.target.name)}>|abs|</button>
-                    {/* </div>
-
-        <div className="boardRowCSS"> */}
                     <button className="sideofsquareCSS" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button>
                     <button className="sideofsquareCSS" name="÷" onClick={e => this.props.onClick(e.target.name)}>÷</button>
                 </div>
@@ -71,7 +73,6 @@ class BoardRC extends React.Component {
                     <button className="sideofsquareCSS" name="π" onClick={e => this.props.onClick(e.target.name)}>π</button>
                     <button className="squareEqual" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
                 </div>
-                
 
                 <div className="boardRowCSS">
                     <button className="sideofsquareCSS" name="sin" onClick={e => this.props.onClick(e.target.name)}>sin</button>
@@ -115,7 +116,7 @@ class ResultRC extends React.Component {
 
 class App extends React.Component {
     constructor() {
-        super();
+        super(); // @ alejandro: why is it sometimes super(props)? is that when it's this.props, not this.state? that confuses me
 
         this.state = {
             resultPROP: ""
@@ -154,7 +155,7 @@ class App extends React.Component {
 
 
     calculateFXN = () => {
-        var isResultGarbage = ''
+        var isResultGarbage = '' // i understand why we're not using const
 
         if (this.state.resultPROP.includes('--')) {
             isResultGarbage = this.state.resultPROP.replace('--', '+')
@@ -164,10 +165,13 @@ class App extends React.Component {
             isResultGarbage = this.state.resultPROP.replace('%', '*100')
         }
 
+        // not super pleased with this pi guy vv
         else if (this.state.resultPROP.includes('π')) {
             isResultGarbage = this.state.resultPROP.replace('*π', '').replace('π', '') * Math.PI
         }
 
+        // I don't really think this is working below vv. -- i want it to remove parentheses if theyre at the beginning or end of the string. 
+        // but that's a problem bc i evaluate them separately, when I really need to have them as a conjunctive event or else it might mess up parentheses in general in the string
         else if (this.state.resultPROP.includes('log')) {
             isResultGarbage = Math.log(this.state.resultPROP.replace('log', '').replace(/^\(/g, '').replace(/\)$/g, ''))
         }
@@ -215,9 +219,12 @@ class App extends React.Component {
 
         try {
             this.setState({
+                // i wrote the replace thing at the end, but i got the logic for the eval part from something online (maybe alejandro knows)
+                //   ˚ and i don't really know why we have: || "") + "")
+                // why does that vv suppress the danger message, alejandro?
                 // eslint-disable-next-line
-                resultPROP: ((eval(isResultGarbage) || "") + "").replace(/^$/, 0)
-            })
+               resultPROP: ((eval(isResultGarbage) || "") + "").replace(/^$/, 0)
+            }) // Line 230:  eval can be harmful  no-eval
 
         } catch (e) {
             this.setState({
@@ -225,6 +232,9 @@ class App extends React.Component {
             })
         }
 
+        // below I wanted to do text input & I was just trying random stuff
+        //  since i dont know javascript's whole personality w/ all its quirks, i have trouble writing stuff that doesn't fail
+        
         // if(button === null && input != null){
         //     this.setState({
         //         resultPROP: input
@@ -239,12 +249,6 @@ class App extends React.Component {
             resultPROP: ""
         })
     };
-
-    // backspace = () => {
-    //     this.setState({
-    //         resultPROP: this.state.resultPROP.slice(0, -1)
-    //     })
-    // };
 
     render() {
         return (
@@ -265,7 +269,7 @@ export default App;
 
 ReactDOM.render(
     <App />,
-    document.getElementById('root')
+    document.getElementById('root') // @ alejandro what is it doing with 'root'? why?
 );
 
 
