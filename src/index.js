0b3d101d7ccde65a235c1033daf075f5f4bda4e6
Samantha Karlaina Rhoads <skr2150@columbia.edu>
2019-08-02 17:52:36 -0400

    taking the recycling out to the curb
    basically just deleting a bunch of commented-out code & trying to name things a little more consistently (but not really)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 
 
 /*
-NOTES:
+NOTES: (+ potentially Qs for alejandro)
 
 - For stuff like log(num), I wanna use a regular expression to capture & erase the log() part, without erasing the digit within, but idk the regex in node js
 - Use text input IN ADDITION to button input, so like typing nums or deleting them & stuff, and updating the input dynamically
-
+- why does it seem like I need to have my board react component class (BoardRC) start with a capital letter? Am i delusional or does it muck up when it's a lowercase b?
+- what are the based regex-ish syntaxes for operations like .replace() & stuff? 
+    ˚ie: "|" vs "||" or "\b" or "[[:alpha:]]" or "^$" or "\\<\\>" & more that seem not to work the same way as they do in Python & R
 */
 
 function factorialFXN(num) {
@@ -24,25 +26,25 @@ class BoardRC extends React.Component {
         return (
             <div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="squareCSS" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
                     <button className="squareCSS" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
                     <button className="squareCSS" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
                 </div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="squareCSS" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
                     <button className="squareCSS" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
                     <button className="squareCSS" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
                 </div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="squareCSS" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
                     <button className="squareCSS" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
                     <button className="squareCSS" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
                 </div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="sideofsquareCSS" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
                     <button className="sideofsquareCSS" name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
                     <button className="squareCSS" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
@@ -50,19 +52,19 @@ class BoardRC extends React.Component {
                     <button className="sideofsquareCSS" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
                 </div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="sideofsquareCSS" name="%" onClick={e => this.props.onClick(e.target.name)}>%</button>
                     <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
                     <button className="sideofsquareCSS" name="!" onClick={e => this.props.onClick(e.target.name)}>!</button>
                     <button className="sideofsquareCSS" name=" abs" onClick={e => this.props.onClick(e.target.name)}>|abs|</button>
                     {/* </div>
 
-        <div className="board-row"> */}
+        <div className="boardRowCSS"> */}
                     <button className="sideofsquareCSS" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button>
                     <button className="sideofsquareCSS" name="÷" onClick={e => this.props.onClick(e.target.name)}>÷</button>
                 </div>
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="sideofsquareCSS" name="√" onClick={e => this.props.onClick(e.target.name)}>√</button>
                     <button className="sideofsquareCSS" name="²" onClick={e => this.props.onClick(e.target.name)}>__²</button>
                     <button className="sideofsquareCSS" name="log" onClick={e => this.props.onClick(e.target.name)}>log</button>
@@ -71,7 +73,7 @@ class BoardRC extends React.Component {
                 </div>
                 
 
-                <div className="board-row">
+                <div className="boardRowCSS">
                     <button className="sideofsquareCSS" name="sin" onClick={e => this.props.onClick(e.target.name)}>sin</button>
                     <button className="sideofsquareCSS" name="cos" onClick={e => this.props.onClick(e.target.name)}>cos</button>
                     <button className="sideofsquareCSS" name="tan" onClick={e => this.props.onClick(e.target.name)}>tan</button>
@@ -79,7 +81,7 @@ class BoardRC extends React.Component {
                           {/* <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button> */}
                     <button className="squareCE" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
                 </div>
-                <div className="board-row"> 
+                <div className="boardRowCSS"> 
                     {/* <input className="squareInputCalculations" type="text" name="title" value={this.state.value} onChange={this.handleChange}></input> */}
                     {/* <input className="squareInputCalculations" type="text" name="title" onChange={e => this.props.onChange(e.target.name)}></input> */}
                 </div>
@@ -126,9 +128,9 @@ class App extends React.Component {
 
     onClick = button => {
 
-        if (button === "=") this.calculate()
+        if (button === "=") this.calculateFXN()
 
-        else if (button === "CE") this.reset()
+        else if (button === "CE") this.resetFXN()
         
         else {
             this.setState({
@@ -146,15 +148,12 @@ class App extends React.Component {
     //     event.preventDefault();
     // }
 
-
-
-    // calculatetxt = () => {
+    // calculateFXNtxt = () => {
     //     var isResultGarbage = ''
-
     // }
 
 
-    calculate = () => {
+    calculateFXN = () => {
         var isResultGarbage = ''
 
         if (this.state.resultPROP.includes('--')) {
@@ -192,11 +191,6 @@ class App extends React.Component {
         else if (this.state.resultPROP.includes('tan')) {
             isResultGarbage = Math.tan(this.state.resultPROP.replace('tan', ''))
         }
-
-
-        // else if (this.state.resultPROP.includes('ln')) {
-        // isResultGarbage = Math.ln(this.state.resultPROP.replace('ln', ''))
-        // }
 
         // else if (this.state.resultPROP.includes('log')) {
         // isResultGarbage = Math.log(this.state.resultPROP.replace('\\([[:digit:]]\\)|log', ''))
@@ -240,7 +234,7 @@ class App extends React.Component {
         
     };
 
-    reset = () => {
+    resetFXN = () => {
         this.setState({
             resultPROP: ""
         })
@@ -255,8 +249,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <div className="calculator-body">
-                    <h1>srhoads' calculator!</h1>
+                <div className="calcbodCSS">
+                    <h2>srhoads' calculator!</h2>
                     <ResultRC resultPROP={this.state.resultPROP} />
                     <BoardRC onClick={this.onClick} />
                 </div>
@@ -291,165 +285,3 @@ ReactDOM.render(
 
 
 
-
-
-
-
-// // ----- LIBRARIES: ----------------
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-
-// class BoardRC extends React.Component { // DATA GETS PASSED FROM BOARD TO SQUARE
-
-
-//   render() {
-//     // const status = "srhoads' calculator!"; // const cat = <b>{`This is cat text ${2 + 1}`}</b>;
-//     // let catpoop = "cleanlitter!"; // const cat = <b>{`This is cat text ${2 + 1}`}</b>;
-//     return (
-//       <div>
-//         <div className="statusCSS">srhoads' calculator!</div>
-
-//         <br></br>
-
-//         <div className="board-row">
-//           <button className="squareCSS" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-//           <button className="squareCSS" name='2' onClick={e => this.props.onClick(e.target.name)}>2</button>
-//           <button className="squareCSS" name='3' onClick={e => this.props.onClick(e.target.name)}>3</button>
-//         </div>
-
-//         <div className="board-row">
-//           <button className="squareCSS" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
-//           <button className="squareCSS" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
-//           <button className="squareCSS" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
-//           <div className="board-row">
-//           </div>
-
-//           <button className="squareCSS" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
-//           <button className="squareCSS" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
-//           <button className="squareCSS" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
-//         </div>
-//         <br></br>
-//         <div className="board-row">
-//           <button className="squareOperators" name='+' onClick={e => this.props.onClick(e.target.name)}>+</button>
-//           <button name="-" className="squareOperators" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
-//           <button className="squareOperators">÷</button>
-//           <button className="squareOperators">%</button>
-//           <button className="squareOperators">(</button>
-//           <button className="squareOperators">)</button>
-//         </div>
-//         <div className="board-row">
-//           <button className="squareOperators">√</button>
-//           <button className="squareCE" name='CE' onClick={e => this.props.onClick(e.target.name)}>CE</button>
-//           <button className="squareEqual" name='=' onClick={e => this.props.onClick(e.target.name)}>=</button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-
-
-// class resultDisplayRC extends React.Component {
-//   render() {
-//     let { resultPROP } = this.props;
-//     return (<div className='resultCSS'>
-//       <p>{resultPROP}</p>
-//     </div>)
-//   }
-// }
-
-
-
-// class GameRC extends React.Component {
-
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       resultPROP: ""
-//     }
-//   }
-
-
-//   onClick = button => {
-
-//     if(button === "="){
-//       this.calculateFXN()
-//     }
-
-//     else if(button === "CE"){
-//       this.reset()
-//     }
-
-//     else {
-//       this.setState({
-//         resultPROP: this.state.resultPROP + button
-//       })
-//     }
-//   };
-
-
-//   calculateFXN = () => {
-//     var isResultGarbage = ''
-//     if(this.state.resultPROP.includes('--')){
-//         isResultGarbage = this.state.resultPROP.replace('--','+')
-//     }
-
-//     else {
-//         isResultGarbage = this.state.resultPROP
-//     }
-
-//     try {
-//       this.setState({
-//         // eslint-disable-next-line
-//         resultPROP: (eval(isResultGarbage) || "") + ""
-//       })
-//     } catch (e) {
-//       this.setState({
-//         resultPROP: "error"
-//       })
-//     }
-//   };
-
-
-//   // calculateFXN = () => {
-//   //   try {
-//   //     this.setState({
-//   //       resultPROP: (eval(this.state.resultPROP) || "") + ""
-//   //     })
-//   //     } catch (e){
-//   //       this.setState({
-//   //         error: "error"
-//   //       })
-//   //     }
-//   //   };
-
-
-
-//   resetFXN = () => {
-//     this.setState({
-//       resultPROP: ""
-//     })
-//   };
-
-//   render() {
-//     return (
-//       <div className="overallappCSS">
-//         <div className="game-board">
-//           <resultDisplayRC resultPROP={this.state.resultPROP}/>
-//           <BoardRC onClick={this.onClick}/>
-//         </div>
-//         <div className="game-info">
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// // --------------------------------------
-
-// ReactDOM.render(
-//   <GameRC />,
-//   document.getElementById('root')
-// );
