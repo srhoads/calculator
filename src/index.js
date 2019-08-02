78b565afe744119267c63f4f62238da80843b153
Samantha Karlaina Rhoads <skr2150@columbia.edu>
2019-08-02 07:58:51 -0400

    Update index.js: added factorial; rearranged buttons

--- src/index.js
+++ src/index.js
@@ -42,72 +42,88 @@ import './index.css';
 // <button className="squareEqual" name='=' onClick={e => this.props.onClick(e.target.name)}>=</button>
 // </div>  
 
+function factorialize(num) {
+    // If the number is less than 0, reject it.
+    if (num < 0)
+        return -1;
+
+    // If the number is 0, its factorial is 1.
+    else if (num === 0)
+        return 1;
+
+    // Otherwise, call the recursive procedure again
+    else {
+        return (num * factorialize(num - 1));
+    }
+}
 
 
 
 class BoardRC extends React.Component {
 
-  render() {
-    return (
-      <div>
 
-        <div className="board-row">
-          <button className="squareCSS" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-          <button className="squareCSS" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
-          <button className="squareCSS" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
-        </div>
 
-        <div className="board-row">
-          <button className="squareCSS" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
-          <button className="squareCSS" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
-          <button className="squareCSS" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
-        </div>
+    render() {
+        return (
+            <div>
 
-        <div className="board-row">
-          <button className="squareCSS" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
-          <button className="squareCSS" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
-          <button className="squareCSS" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
-        </div>
+                <div className="board-row">
+                    <button className="squareCSS" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                    <button className="squareCSS" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                    <button className="squareCSS" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                </div>
 
-        <div className="board-row">
-          <button className="sideofsquareCSS" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-          <button className="sideofsquareCSS" name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-          <button className="squareCSS" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
-          <button className="sideofsquareCSS" name="%" onClick={e => this.props.onClick(e.target.name)}>%</button>
-          <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
-        </div>
+                <div className="board-row">
+                    <button className="squareCSS" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                    <button className="squareCSS" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                    <button className="squareCSS" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                </div>
 
-        <div className="board-row">
-          <button className="sideofsquareCSS" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
-          <button className="sideofsquareCSS" name="+" onClick={e => this.props.onClick(e.target.name)}>+</button>
-          <button className="sideofsquareCSS" name="!" onClick={e => this.props.onClick(e.target.name)}>!</button>
-          <button className="sideofsquareCSS" name=" abs" onClick={e => this.props.onClick(e.target.name)}>|abs|</button>
-          {/* </div>
+                <div className="board-row">
+                    <button className="squareCSS" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                    <button className="squareCSS" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                    <button className="squareCSS" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                </div>
+
+                <div className="board-row">
+                    <button className="sideofsquareCSS" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
+                    <button className="sideofsquareCSS" name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+                    <button className="squareCSS" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                    <button className="sideofsquareCSS" name="+" onClick={e => this.props.onClick(e.target.name)}>+</button>
+                    <button className="sideofsquareCSS" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button>
+                </div>
+
+                <div className="board-row">
+                    <button className="sideofsquareCSS" name="%" onClick={e => this.props.onClick(e.target.name)}>%</button>
+                    <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                    <button className="sideofsquareCSS" name="!" onClick={e => this.props.onClick(e.target.name)}>!</button>
+                    <button className="sideofsquareCSS" name=" abs" onClick={e => this.props.onClick(e.target.name)}>|abs|</button>
+                    {/* </div>
 
         <div className="board-row"> */}
-          <button className="sideofsquareCSS" name="√" onClick={e => this.props.onClick(e.target.name)}>√</button>
-          <button className="sideofsquareCSS" name="²" onClick={e => this.props.onClick(e.target.name)}>__²</button>
-        </div>
+                    <button className="sideofsquareCSS" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button>
+                    <button className="sideofsquareCSS" name="÷" onClick={e => this.props.onClick(e.target.name)}>÷</button>
+                </div>
 
-        <div className="board-row">
-          <button className="sideofsquareCSS" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button>
-          <button className="sideofsquareCSS" name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button>
-          <button className="sideofsquareCSS" name="sin" onClick={e => this.props.onClick(e.target.name)}>sin</button>
-          <button className="sideofsquareCSS" name="cos" onClick={e => this.props.onClick(e.target.name)}>cos</button>
-          <button className="squareEqual" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
-        </div>
+                <div className="board-row">
+                    <button className="sideofsquareCSS" name="√" onClick={e => this.props.onClick(e.target.name)}>√</button>
+                    <button className="sideofsquareCSS" name="²" onClick={e => this.props.onClick(e.target.name)}>__²</button>
+                    <button className="sideofsquareCSS" name="log" onClick={e => this.props.onClick(e.target.name)}>log</button>
+                    <button className="sideofsquareCSS" name="π" onClick={e => this.props.onClick(e.target.name)}>π</button>
+                    <button className="squareEqual" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                </div>
 
-        <div className="board-row">
-          <button className="sideofsquareCSS" name="tan" onClick={e => this.props.onClick(e.target.name)}>tan</button>
-          {/* <button className="sideofsquareCSS" name="ln" onClick={e => this.props.onClick(e.target.name)}>ln</button> */}
-          <button className="sideofsquareCSS" name="log" onClick={e => this.props.onClick(e.target.name)}>log</button>
-          <button className="sideofsquareCSS" name="π" onClick={e => this.props.onClick(e.target.name)}>π</button>
-          {/* <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button> */}
-          <button className="squareCE" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
-        </div>
-      </div>
-    );
-  }
+                <div className="board-row">
+                    <button className="sideofsquareCSS" name="sin" onClick={e => this.props.onClick(e.target.name)}>sin</button>
+                    <button className="sideofsquareCSS" name="cos" onClick={e => this.props.onClick(e.target.name)}>cos</button>
+                    <button className="sideofsquareCSS" name="tan" onClick={e => this.props.onClick(e.target.name)}>tan</button>
+                    {/* <button className="sideofsquareCSS" name="ln" onClick={e => this.props.onClick(e.target.name)}>ln</button> */}
+                          {/* <button className="sideofsquareCSS" name="." onClick={e => this.props.onClick(e.target.name)}>.</button> */}
+                    <button className="squareCE" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 
@@ -118,15 +134,15 @@ class BoardRC extends React.Component {
 class ResultRC extends React.Component {
 
 
-  render() {
-      let {resultPROP} = this.props;
-      return (
-          <div className="resultCSS">
-              <p>{resultPROP}</p>
-          </div>
-  )
-      ;
-  }
+    render() {
+        let { resultPROP } = this.props;
+        return (
+            <div className="resultCSS">
+                <p>{resultPROP}</p>
+            </div>
+        )
+            ;
+    }
 }
 
 
@@ -138,136 +154,136 @@ class ResultRC extends React.Component {
 
 
 class App extends React.Component {
-  constructor(){
-      super();
+    constructor() {
+        super();
 
-      this.state = {
-          resultPROP: ""
-      }
-  }
-
-  onClick = button => {
-
-      if(button === "="){
-          this.calculate()
-      }
-
-      else if(button === "CE"){
-          this.reset()
-      }
-      // else if(button === "CE"){
-      //     this.backspace()
-      // }
-
-      else {
-          this.setState({
-              resultPROP: this.state.resultPROP + button
-          })
-      }
-  };
-
-
-  calculate = () => {
-    var checkResult = ''
-
-    if (this.state.resultPROP.includes('--')) {
-      checkResult = this.state.resultPROP.replace('--', '+')
+        this.state = {
+            resultPROP: ""
+        }
     }
 
-    else if (this.state.resultPROP.includes('%')) {
-      checkResult = this.state.resultPROP.replace('%', '*100')
+    onClick = button => {
+
+        if (button === "=") {
+            this.calculate()
+        }
+
+        else if (button === "CE") {
+            this.reset()
+        }
+        // else if(button === "CE"){
+        //     this.backspace()
+        // }
+
+        else {
+            this.setState({
+                resultPROP: this.state.resultPROP + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+
+        if (this.state.resultPROP.includes('--')) {
+            checkResult = this.state.resultPROP.replace('--', '+')
+        }
+
+        else if (this.state.resultPROP.includes('%')) {
+            checkResult = this.state.resultPROP.replace('%', '*100')
+        }
+
+        else if (this.state.resultPROP.includes('π')) {
+            checkResult = this.state.resultPROP.replace('*π', '').replace('π', '') * Math.PI
+        }
+
+        else if (this.state.resultPROP.includes('log')) {
+            checkResult = Math.log(this.state.resultPROP.replace('log', '').replace(/^\(/g, '').replace(/\)$/g, ''))
+        }
+
+        else if (this.state.resultPROP.includes('abs')) {
+            checkResult = Math.abs(this.state.resultPROP.replace('abs', '').replace(/\|/g, ''))
+        }
+
+        // else if (this.state.resultPROP.includes('ln')) {
+        //   checkResult = Math.LN10(this.state.resultPROP.replace('ln', ''))
+        // }
+
+        else if (this.state.resultPROP.includes('sin')) {
+            checkResult = Math.sin(this.state.resultPROP.replace('sin', ''))
+        }
+
+        else if (this.state.resultPROP.includes('cos')) {
+            checkResult = Math.cos(this.state.resultPROP.replace('cos', ''))
+        }
+
+        else if (this.state.resultPROP.includes('tan')) {
+            checkResult = Math.tan(this.state.resultPROP.replace('tan', ''))
+        }
+
+
+        // else if (this.state.resultPROP.includes('ln')) {
+        // checkResult = Math.ln(this.state.resultPROP.replace('ln', ''))
+        // }
+
+        // else if (this.state.resultPROP.includes('log')) {
+        // checkResult = Math.log(this.state.resultPROP.replace('\\([[:digit:]]\\)|log', ''))
+        // }
+
+        else if (this.state.resultPROP.includes('√')) {
+            checkResult = Math.sqrt(this.state.resultPROP.replace('√', ''))
+        }
+
+        else if (this.state.resultPROP.includes('²')) {
+            checkResult = this.state.resultPROP.replace('²', '') * this.state.resultPROP.replace('²', '')
+        }
+
+        else if (this.state.resultPROP.includes('!')) {
+            checkResult = factorialize(this.state.resultPROP.replace('!', ''))
+        }
+
+
+        else {
+            checkResult = this.state.resultPROP
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                resultPROP: (eval(checkResult) || "") + ""
+            })
+        } catch (e) {
+            this.setState({
+                resultPROP: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            resultPROP: ""
+        })
+    };
+
+    // backspace = () => {
+    //     this.setState({
+    //         resultPROP: this.state.resultPROP.slice(0, -1)
+    //     })
+    // };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>srhoads' calculator</h1>
+                    <ResultRC resultPROP={this.state.resultPROP} />
+                    <BoardRC onClick={this.onClick} />
+                </div>
+            </div>
+        );
     }
-
-    else if (this.state.resultPROP.includes('π')) {
-      checkResult = this.state.resultPROP.replace('*π', '').replace('π', '') * Math.PI
-    }
-
-    else if (this.state.resultPROP.includes('log')) {
-      checkResult = Math.log(this.state.resultPROP.replace('log', '').replace(/^\(/g, '').replace(/\)$/g,''))
-    }
-
-    else if (this.state.resultPROP.includes('abs')) {
-      checkResult = Math.abs(this.state.resultPROP.replace('abs', '').replace(/\|/g,''))
-    }
-
-    // else if (this.state.resultPROP.includes('ln')) {
-    //   checkResult = Math.LN10(this.state.resultPROP.replace('ln', ''))
-    // }
-
-    else if (this.state.resultPROP.includes('sin')) {
-      checkResult = Math.sin(this.state.resultPROP.replace('sin', ''))
-    }
-
-    else if (this.state.resultPROP.includes('cos')) {
-      checkResult = Math.cos(this.state.resultPROP.replace('cos', ''))
-    }
-
-    else if (this.state.resultPROP.includes('tan')) {
-      checkResult = Math.tan(this.state.resultPROP.replace('tan', ''))
-    }
-
-
-    // else if (this.state.resultPROP.includes('ln')) {
-      // checkResult = Math.ln(this.state.resultPROP.replace('ln', ''))
-    // }
-
-    // else if (this.state.resultPROP.includes('log')) {
-      // checkResult = Math.log(this.state.resultPROP.replace('\\([[:digit:]]\\)|log', ''))
-    // }
-
-    else if (this.state.resultPROP.includes('√')) {
-      checkResult = Math.sqrt(this.state.resultPROP.replace('√', ''))
-    }
-
-    else if (this.state.resultPROP.includes('²')) {
-      checkResult = this.state.resultPROP.replace('²', '') * this.state.resultPROP.replace('²', '')
-    }
-
-    else if (this.state.resultPROP.includes('x!')) {
-      checkResult = this.state.resultPROP.replace('x!', '!') * this.state.resultPROP.replace('²', '')
-    }
-
-
-      else {
-          checkResult = this.state.resultPROP
-      }
-
-      try {
-          this.setState({
-              // eslint-disable-next-line
-              resultPROP: (eval(checkResult) || "" ) + ""
-          })
-      } catch (e) {
-          this.setState({
-              resultPROP: "error"
-          })
-
-      }
-  };
-
-  reset = () => {
-      this.setState({
-          resultPROP: ""
-      })
-  };
-
-  // backspace = () => {
-  //     this.setState({
-  //         resultPROP: this.state.resultPROP.slice(0, -1)
-  //     })
-  // };
-
-  render() {
-      return (
-          <div>
-              <div className="calculator-body">
-                  <h1>srhoads' calculator</h1>
-                  <ResultRC resultPROP={this.state.resultPROP}/>
-                  <BoardRC onClick={this.onClick}/>
-              </div>
-          </div>
-      );
-  }
 }
 
 export default App;
@@ -275,8 +291,8 @@ export default App;
 
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
 
